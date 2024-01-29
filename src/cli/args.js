92cb2b9b2fd05c9cd73d9dd7e5ef8b000808fc14
makzhentos <makzhentos@gmail.com>
2024-01-29 15:55:28 +0300

    feat: add cli module colution to project

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import process from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const result = []
+    const args = process.argv;
+    args.map((el, i, arr) => {
+        if (el.startsWith('--')){
+            result.push(`${el.slice(2)} is ${arr[i+1]}`)
+        }
+    })
+    console.log(result.join(', '));
 };
 
 parseArgs();
